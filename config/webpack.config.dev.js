4679bd174137f68bacc8d722f4a5391ac0a0ada6
Andrew Errico <andrew@sportsyou.com>
2017-09-30 18:37:45 -0400

    setup linting, editerconfig, prettier

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,21 @@
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const getClientEnvironment = require('./env');
+const paths = require('./paths');
+
+// Serve project from root for development
+const publicPath = '/';
+const publicUrl = '';
+const env = '';
+
+module.exports = {
+  devtool: 'eval', // 'cheap-module-source-map'
+  entry: [paths]
+};
